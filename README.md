dd71422e6494371e93ba60007b5936385555667c
woogi95 <sdw951216@gmail.com>
2025-04-03 10:56:33 +0900

    재배포

--- README.md
+++ README.md
@@ -49,4 +49,46 @@ export const metadata: Metadata = {
 
 - https://vercel.com/
 - 환경변수 등록 주의
-- SITE_URL 은 로그인 이후 이동할 주소 이므로 주의
+  - `SITE_URL 은 로그인 이후 이동할 주소 이므로 주의`
+  - `https://til-supabase-steel.vercel.app/`
+
+# 배포 에러 처리
+
+```js
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+// Prettier 플러그인 추가
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      prettier: eslintPluginPrettier, //  Prettier 플러그인 추가
+    },
+    rules: {
+      ...eslintConfigPrettier.rules, //  Prettier와 충돌하는 ESLint 규칙 비활성화
+      "prettier/prettier": ["off", { endOfLine: "auto" }], //  Prettier 스타일을 강제 적용 (오류 발생 시 ESLint에서 표시)
+      "@typescript-eslint/no-unused-vars": "off", //  기존 TypeScript 규칙 유지
+      "@typescript-eslint/no-explicit-any": "off", //  any 타입 사용 허용
+      "react-hooks/exhaustive-deps": "off",
+    },
+  },
+];
+
+export default eslintConfig;
+```
+
+# 구글 로그인 후 RedirectURL 설정
+
+- https://cloud.google.com/developers?hl=ko
--- src/components/auth/loginform.tsx
+++ src/components/auth/loginform.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { signInWithGoogle, signInWithKakao } from "@/lib/supabase/action";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -25,7 +26,6 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { signInWithGoogle, signInWithKakao } from "@/lib/supabase/action";
 
 const formSchema = z.object({
   email: z.string().email({
@@ -85,6 +85,7 @@ export function LoginForm() {
       setIsLoading(false);
     }
   };
+
   const handleKakaoLogin = async () => {
     try {
       setIsLoading(true);
@@ -165,24 +166,27 @@ export function LoginForm() {
             </span>
           </div>
         </div>
-        <Button
-          variant="outline"
-          type="button"
-          className="w-full"
-          onClick={handleGoogleLogin}
-          disabled={isLoading}
-        >
-          Google로 계속하기
-        </Button>
-        <Button
-          variant="outline"
-          type="button"
-          className="w-full"
-          onClick={handleKakaoLogin}
-          disabled={isLoading}
-        >
-          카카오로 계속하기
-        </Button>
+
+        <form action={signInWithGoogle} className="w-full">
+          <Button
+            variant="outline"
+            type="submit"
+            className="w-full"
+            disabled={isLoading}
+          >
+            Google로 계속하기
+          </Button>
+        </form>
+        <form action={signInWithKakao} className="w-full">
+          <Button
+            variant="outline"
+            type="submit"
+            className="w-full"
+            disabled={isLoading}
+          >
+            Kakao로 계속하기
+          </Button>
+        </form>
         <div className="text-center text-sm text-muted-foreground">
           계정이 없으신가요?{" "}
           <Button
