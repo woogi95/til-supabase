26644b75806604afcc8382241f9b8ab5279255b5
woogi95 <sdw951216@gmail.com>
2025-04-03 11:03:00 +0900

    재배포 3

--- src/lib/supabase/action.ts
+++ src/lib/supabase/action.ts
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation";
 
 const signInWith = (provider: Provider) => async () => {
   const supabase = await createServerSideClient();
-
+  console.log(`${process.env.SITE_URL}/auth/callback`);
   const auth_callback_url = `${process.env.SITE_URL}/auth/callback`;
 
   const { data, error } = await supabase.auth.signInWithOAuth({
